17edafcabd6e5075043c47c70f1f3ffc46fa88cd
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-28 19:24:32 +0800

    Brew formula update for timber version v0.1.1

--- Formula/timber.rb
+++ Formula/timber.rb
@@ -5,21 +5,21 @@
 class Timber < Formula
   desc "A tool to extract translations from CSV file and append to the strings resources in your Android project"
   homepage "https://github.com/master-g/timber"
-  version "0.1.0"
+  version "0.1.1"
   license "Apache License"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/master-g/timber/releases/download/v0.1.0/timber_0.1.0_darwin_arm64.zip"
-      sha256 "10fd5145d99f18b9c3452634be3c4ce3e18f2b55b9b8fd3463ee75fa130bfa3e"
+      url "https://github.com/master-g/timber/releases/download/v0.1.1/timber_0.1.1_darwin_arm64.zip"
+      sha256 "7d62c22b81e0855adbe62b3c5a38ca31b6307e6fbb73e8d5356d5aa92285d724"
 
       def install
         bin.install "timber"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/master-g/timber/releases/download/v0.1.0/timber_0.1.0_darwin_amd64.zip"
-      sha256 "2524d30fed78ca1c403c62704037740d95aa0136b47293c233744f6f0d3ecbc9"
+      url "https://github.com/master-g/timber/releases/download/v0.1.1/timber_0.1.1_darwin_amd64.zip"
+      sha256 "e6a35cedeb1cd875b98c46c8ce2832996ccedddb2aa543f509a8ca00853ac424"
 
       def install
         bin.install "timber"
@@ -29,16 +29,16 @@ class Timber < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/master-g/timber/releases/download/v0.1.0/timber_0.1.0_linux_arm64.zip"
-      sha256 "d10000b612a3e0e67571facb0545dfce37b00a96c2981167ac1371109ba5f21f"
+      url "https://github.com/master-g/timber/releases/download/v0.1.1/timber_0.1.1_linux_arm64.zip"
+      sha256 "138483b86882351821348654fab4e5e5bd8641697b76ed6bf38506d14dd9a8ee"
 
       def install
         bin.install "timber"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/master-g/timber/releases/download/v0.1.0/timber_0.1.0_linux_amd64.zip"
-      sha256 "7bfdb45379ff304b3a7e9244c2191723da6c99f0a9154371ec640464befef21b"
+      url "https://github.com/master-g/timber/releases/download/v0.1.1/timber_0.1.1_linux_amd64.zip"
+      sha256 "4632eb7c6d5ae4277a1f2fbac7a9f25765efe93a5be0cadbfe8caad402b54da4"
 
       def install
         bin.install "timber"
